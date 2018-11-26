5009ed420bc35dd3064ea5c981788cdf9a6a2b02
kolodziejczakM <kolodziejczak.mn@gmail.com>
2018-11-26 01:24:18 +0100

    Core code changes. Dropdown works.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
 import { LitElement, html } from '@polymer/lit-element';
 
+// Declaring custom types (here handling object / array passed in HTML) - basically: innerHTML rendering support
+const JsonType = {
+    fromAttribute: attr => {
+        return typeof attr === 'string' ? JSON.parse(attr) : attr;
+    },
+    toAttribute: prop => {
+        return typeof prop !== 'string' ? JSON.stringify(prop) : prop;
+    }
+};
+
 class FeaturedDropdown extends LitElement {
     static get properties() {
         return {
-            chosenOption: { type: Object }
+            chosenOption: {
+                type: JsonType,
+                attribute: 'chosen-option',
+                // reflect: true, // two way data binding possibility
+                hasChanged(newVal, oldVal) {
+                    if (oldVal === undefined) {
+                        return true;
+                    }
+                    return newVal.value !== oldVal.value;
+                }
+            },
+            options: {
+                type: JsonType
+            },
+            onChangeHandler: {
+                type: Function,
+                attribute: 'on-change-handler'
+            },
+            isExpanded: {
+                type: Boolean,
+                attribute: 'is-expanded'
+            }
         };
     }
 
     constructor() {
         super();
+
+        // defaultProps / state values
         this.options = [];
         this.chosenOption = { label: '(no options)', value: null };
         this.isExpanded = false;
+
+        this.onOutsideClick = this.onOutsideClick.bind(this);
+    }
+
+    onExpandClick() {
+        if (this.options.length) {
+            this.isExpanded = true;
+        }
+    }
+
+    collapse() {
+        this.isExpanded = false;
+    }
+
+    onChange(event) {
+        const selectedOption = JSON.parse(event.target.getAttribute('value'));
+
+        this.chosenOption = selectedOption;
+        this.collapse();
+
+        this.onChangeHandler(selectedOption);
+        this.dispatchEvent(
+            new CustomEvent('dropdownValueChanged', { detail: selectedOption })
+        );
+    }
+
+    firstUpdated() {
+        document.addEventListener('click', this.onOutsideClick);
+    }
+
+    // Native api callbacks support
+    disconnectedCallback() {
+        document.removeEventListener('click', this.onOutsideClick);
+    }
+
+    onOutsideClick(event) {
+        if (event.target !== this && !event.target.slot) {
+            this.collapse();
+        }
     }
 
     render() {
@@ -76,12 +148,25 @@ class FeaturedDropdown extends LitElement {
                 }
             </style>
             <div class="dropdown">
-                <div class="selected-option">
-                    <span class="selected-option__label"><!-- dynamic render--></span>
+                <div @click="${this.onExpandClick}" class="selected-option">
+                    <span class="selected-option__label">${this.chosenOption.label}</span>
                     <slot name="dropdown-arrow">Your icon here</slot>
                 </div>
-                <ul class="options">
-                    <!-- dynamic render -->
+                <ul class="${this.isExpanded ? 'options--expanded' : 'options'}">
+                    ${
+                        this.options.map(
+                            option =>
+                                html`
+                                    <option
+                                        @click="${this.onChange}"
+                                        class="option"
+                                        value="${JSON.stringify(option)}"
+                                    >
+                                        ${option.label}
+                                    </option>
+                                `
+                        )
+                    }
                 </ul>
             </div>
         `;
